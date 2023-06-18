8cd84b2903128282fdd41bf8ffc3d1f0ca38b2f6
Jesus M <warloy989@gmail.com>
2023-06-17 22:42:07 -0400

    013 - Minor fixes on prompt command description

--- src/commands/unknown.ts
+++ src/commands/unknown.ts
@@ -8,9 +8,15 @@ export const unknown = async (ctx: Context, text: string) => {
   const firstName = ctx.message?.from.first_name ?? ''
   const stickerID = process.env.STICKER_POMPOM_NEGATIVE ?? ''
   
-  let message = `${firstName}, no puedo comprender a qué te refieres con "${text}".`
-  
-  debug(`Triggered "unknown" command with message \n${message}`)
+  let message = ''
+
+  if (text === `/prompt`) {
+    message = `Para usar /prompt, añade tu solicitud seguida del comando, así:\n/prompt <tu solicitud>\n\nPor ejemplo:\n/prompt ¿Por qué las papas fritas se conocen como papas a la francesa?`
+  } else {
+    message = `${ firstName }, no puedo comprender a qué te refieres con "${text}".`
+  }
+
+  debug(`Triggered "unknown" command with message \n${ message } `)
 
   await ctx.replyWithMarkdownV2(message, {
     parse_mode: 'Markdown'
